be28f25d61bb573ead36c4724aeb084f23902660
Adam Walls <awalls1@gmail.com>
2017-08-22 13:51:44 -0700

    included static routes, wrote new html route, logged port to console

--- starter-code/server.js
+++ starter-code/server.js
@@ -4,6 +4,7 @@
 
 // DONE: Require the Express package that you installed via NPM, and instantiate the app
 const express = require('express');
+const app = express();
 
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
@@ -11,10 +12,12 @@ const express = require('express');
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
-
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
+// DONE: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
+// DONE: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/new',function(req,res){
+  res.sendFile('public/new.html', {root: '.'});
+});
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
@@ -25,5 +28,6 @@ app.post('/articles', bodyParser, function(request, response) {
 })
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  // DONE: Log to the console a message that lets you know which port your server has started on
+  console.log(`Our app is listening to port ${PORT}`);
 });
